2293ad62c3d75ac24beb1e4069cc03d89d3acbf0
Anandhan <anandhan.rajagopal@syncfusion.com>
2023-09-14 17:36:34 +0530

    Added Getting Started Sample for SfButton control

---
+++ ButtonGettingStarted/ButtonGettingStarted/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ButtonGettingStarted
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ButtonGettingStarted/ButtonGettingStarted/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ButtonGettingStarted
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
